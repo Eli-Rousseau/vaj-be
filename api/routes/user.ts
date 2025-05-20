edd5829884b0907aca2a0b88cb9b06bb3c57b644
Eli Rousseau <eli@risolto.be>
2025-05-21 00:14:27 +0200

    Modyfing the userRouter api endpoint

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,29 +1,69 @@
 import express from "express";
 import { Router } from "express";
 
-import { pgClient } from "../server";
-
-// import { User, Address } from "../types/classes";
+import { pgClient, ExpectedRequest } from "../server";
+import { User, Address } from "../types/types";
+import { error } from "console";
+import { off } from "process";
 
 const userRouter: Router = express.Router();
 
-userRouter.param("id", (req, res, next, value) => {
-  (req as any).id = value;
-  next();
-});
+// Handeling the route parameters
+userRouter.param(
+  "id",
+  (req: ExpectedRequest, res: express.Response, next, value) => {
+    req.id = value;
+    next();
+  }
+);
 
 userRouter
   .route("/:id")
-  .get((req, res) => {})
-  .post((req, res) => {})
-  .put((req, res) => {})
-  .delete((req, res) => {});
+  .get((req: ExpectedRequest, res: express.Response) => {})
+  .post((req: ExpectedRequest, res: express.Response) => {})
+  .put((req: ExpectedRequest, res: express.Response) => {})
+  .delete((req: ExpectedRequest, res: express.Response) => {});
 
+// Handle request on the main route
 userRouter
   .route("/")
-  .get((req, res) => {})
-  .post((req, res) => {})
-  .put((req, res) => {})
-  .delete((req, res) => {});
+  .get(async (req: ExpectedRequest, res: express.Response) => {
+    // Format the database query
+    const limit: string | undefined = req.limit;
+    const offset: string | undefined = req.offset;
+    const query: string = `
+SELECT *
+FROM shop.user
+ORDER BY updated_at DESC
+${limit ? "LIMIT " + limit + (offset ? "OFFSET " + offset : "") : ""}
+    `;
+
+    // Make connection to the database
+    try {
+      await pgClient.connect();
+    } catch (error) {
+      res.status(500).json({
+        // TO DO: check the status to return
+        error: `Failed to establish connection to database: ${error}`,
+      });
+    }
+
+    // Run the query on the database
+    try {
+      const result = await pgClient.query(query);
+    } catch (error) {
+      res.status(500).json({
+        // TO DO: check the status to return
+        error: `Failed to execute the query on the database: ${error}`,
+        query: query,
+      });
+    }
+  })
+
+  // TO DO: disconnect from database
+
+  .post((req: ExpectedRequest, res: express.Response) => {})
+  .put((req: ExpectedRequest, res: express.Response) => {})
+  .delete((req: ExpectedRequest, res: express.Response) => {});
 
 export default userRouter;
--- api/server.ts
+++ api/server.ts
@@ -4,18 +4,26 @@ import { Client } from "pg";
 
 import { loadStage } from "../utils/stage";
 import userRouter from "./routes/user";
+import { off } from "process";
+
+// Extend the request interface
+interface ExpectedRequest extends express.Request {
+  id?: string;
+  limit?: string;
+  offset?: string;
+}
 
 // Loading the stage variable
 loadStage();
 
 // Import the environment variables
-const database: string = process.env.DATABASE_NAME || "vintage_archive_jungle";
-const host: string = process.env.DATABASE_HOST || "localhost";
+const database: string = process.env.DATABASE_VAJ || "";
+const host: string = process.env.DATABASE_HOST || "";
 const port: number = !isNaN(Number(process.env.DATABASE_PORT))
   ? Number(process.env.DATABASE_PORT)
-  : 5432;
-const user: string = process.env.DATABASE_USER || "administrator";
-const password: string = process.env.DATABASE_PASSWORD || "my_password";
+  : 0;
+const user: string = process.env.DATABASE_ADMINISTRATOR_USER_NAME || "";
+const password: string = process.env.DATABASE_ADMINISTRATOR_USER_PASSWORD || "";
 
 // Initiate the pg client
 const pgClient: Client = new Client({
@@ -30,10 +38,29 @@ const pgClient: Client = new Client({
 const app: Express = express();
 const PORT: number = 1111;
 
+// Handeling the query parameters
+app.use((req: ExpectedRequest, res: express.Response, next) => {
+  // Handeling the limit query parameter
+  let limit: string | undefined = undefined;
+  if (typeof req.query.limit === "string") {
+    limit = req.query.limit;
+  }
+  req.limit = limit;
+
+  // Handeling the offset query parameter
+  let offset: string | undefined = undefined;
+  if (typeof req.query.offset === "string") {
+    offset = req.query.offset;
+  }
+  req.offset = offset;
+
+  next();
+});
+
 // Adding the routers
 app.use("/user", userRouter);
 
 // Start the listen process
 app.listen(PORT);
 
-export { pgClient };
+export { pgClient, ExpectedRequest };
