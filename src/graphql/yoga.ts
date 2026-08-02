0c8979d60a158f569f1f41d92686c23181dea2d1
Eli-Rousseau <rousseau.eli2000@gmail.com>
2026-08-02 14:30:32 +0200

    Working on database permissions

--- src/graphql/yoga.ts
+++ src/graphql/yoga.ts
@@ -2,8 +2,9 @@ import path from "path";
 import { createYoga } from "graphql-yoga";
 import type { GraphQLSchema } from "graphql";
 
-import { logger } from "../utils/logger";
-import { buildGraphQLSchema } from "./build-schema";
+import { logger } from "@/src/utils/logger";
+import { buildGraphQLSchema } from "@/src/graphql/build-schema";
+import { AuthVAJ } from "@/src/api/auth";
 
 const LOGGER = logger.get({
     source: "src",
@@ -19,7 +20,22 @@ export const yoga = createYoga({
       throw new Error("GraphQL schema not initialized");
     }
     return currentSchema;
-  }
+  },
+
+  context: async ({ request }) => {
+    let authorization = request.headers.get("authorization");
+
+    if (
+      !authorization &&
+      process.env.STAGE === "dev"
+    ) {
+      const accessToken = (
+        await AuthVAJ.connectAsDefaultUser()
+      )?.accessToken || "";
+
+      request.headers.set("authorization", accessToken);
+    }
+  },
 });
 
 export async function initSchema() {
